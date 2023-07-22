e0637f5ebf8ad77a21d45f9bbe499ce7cf785335
Sergio Medeiros <sergiof11@outlook.com.br>
2023-07-22 09:53:36 -0300

    Deletando diretório

--- angular-e-spring-full-stack-master/clientes/src/main/java/io/github/dougllasfps/clientes/exception/UsuarioCadastradoException.java
+++
@@ -1,8 +0,0 @@
-package io.github.dougllasfps.clientes.exception;
-
-public class UsuarioCadastradoException extends RuntimeException {
-
-    public UsuarioCadastradoException( String login ){
-        super("Usuário já cadastrado para o login " + login);
-    }
-}
